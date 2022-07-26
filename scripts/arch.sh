304f1e9a9852f359c15c2604a9fe0aaee06a2ab9
Anshuman Biswas <anshuman@biswas.me>
2022-07-26 16:14:57 +0000

    Adding more arch dependencies

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -64,3 +64,43 @@ cat <<EOT >> ~/.custom.zsh
 [[ \$(fgconsole 2>/dev/null) == 1 ]] && exec startx -- vt1
 xrandr -s $RESOLUTION
 EOT
+
+yay -S google-chrome
+
+doas -- pacman -S docker
+
+doas systemctl start docker.service
+doas systemctl enable docker.service
+sudo usermod -aG docker $1
+
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install microk8s --classic
+sudo usermod --append --groups microk8s $1
+microk8s start
+sudo snap enable microk8s
+microk8s kubectl get nodes
+microk8s.kubectl run httpd --image httpd
+microk8s kubectl get services
+microk8s.enable registry
+microk8s.config
+microk8s kubectl delete pod httpd
+microk8s kubectl get pods
+microk8s kubectl -n kube-system get secret
+token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+microk8s.enable dashboard dns
+microk8s.enable dashboard dnsmicrok8s kubectl create token -n kube-system default --duration=8544h
+microk8s kubectl -n kube-system describe secret $token
+microk8s.enable storage
+microk8s.enable prometheus
+# sudo firewall-cmd --zone=public --add-port 9090/tcp --permanent
+# sudo firewall-cmd --zone=public --add-port 3000/tcp --permanent
+microk8s enable helm
+microk8s enable ingress
+yay -S helm
+
+# microk8s kubectl port-forward -n monitoring service/grafana --address 0.0.0.0 3000:3000
+# microk8s kubectl port-forward -n monitoring service/grafana --address 0.0.0.0 9090:9090
